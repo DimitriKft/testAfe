6b610018a3cec5ae28811d4f3fd8e0509ab2378f
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-01-29 14:09:41 +0100

    fc

---
+++ TestAFE/TestAFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAFEApp.swift
+//  TestAFE
+//
+//  Created by dimitri on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
